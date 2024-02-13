1e95d5b1f9caea6f34ef66d300f2def5c722adf5
Alexey Mulyukin <adiott52@gmail.com>
2024-02-13 14:43:43 +0300

    5 ДЗ выполнено

---
+++ src/main/java/ru/mulyukin/java/basic/homework/part2/homework5/Cat.java
@@ -0,0 +1,15 @@
+package ru.mulyukin.java.basic.homework.part2.homework5;
+
+public class Cat extends Animal {
+    public Cat(String name, int runningSpeedMS, int enduranceUE) {
+        super(name, runningSpeedMS, enduranceUE);
+    }
+
+    @Override
+    public int swim(int distance) {
+        System.out.println(name + " плавать не умеет");
+        return distance;
+    }
+
+
+}
---
+++ src/main/java/ru/mulyukin/java/basic/homework/part2/homework5/Horse.java
@@ -0,0 +1,7 @@
+package ru.mulyukin.java.basic.homework.part2.homework5;
+
+public class Horse extends Animal {
+    public Horse(String name, int runningSpeedMS, int enduranceUE, int swimSpeedMS, int energyExpended) {
+        super(name, runningSpeedMS, enduranceUE, swimSpeedMS, energyExpended);
+    }
+}
