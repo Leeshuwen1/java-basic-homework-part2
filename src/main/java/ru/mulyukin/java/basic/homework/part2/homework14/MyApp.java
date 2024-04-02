6b13a5e7937e34a87b3947186902ab7ecb5e2a23
Alexey Mulyukin <adiott52@gmail.com>
2024-04-02 06:08:41 +0300

    14 выполнено

---
+++ src/main/java/ru/mulyukin/java/basic/homework/part2/homework14/MyApp.java
@@ -0,0 +1,68 @@
+package ru.mulyukin.java.basic.homework.part2.homework14;
+
+public class MyApp {
+    public static void main(String[] args) throws InterruptedException {
+        long time1 = System.currentTimeMillis();
+        arraySpeed(new double[100_000_000]);
+        long time2 = System.currentTimeMillis();
+        System.out.println("Время на операцию обычного цикла " + (time2 - time1));
+
+        long time3 = System.currentTimeMillis();
+        arraySpeedThred(new double[100_000_000]);
+        long time4 = System.currentTimeMillis();
+        System.out.println("Время на операцию многопоточного цикла " + (time4 - time3));
+
+
+    }
+
+    private static void arraySpeed(double[] arr) {
+        for (int i = 0; i < 100_000_000; i++) {
+            arr[i] = 1.14 * Math.cos(i) * Math.sin(i * 0.2) * Math.cos(i / 1.2);
+        }
+    }
+
+    private static synchronized void arraySpeedThred(double[] arr) throws InterruptedException {
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 250_000; i++) {
+                    arr[i] = 1.14 * Math.cos(i) * Math.sin(i * 0.2) * Math.cos(i / 1.2);
+                }
+            }
+        });
+        Thread thread1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 250_000; i < 500_000; i++) {
+                    arr[i] = 1.14 * Math.cos(i) * Math.sin(i * 0.2) * Math.cos(i / 1.2);
+                }
+            }
+        });
+        Thread thread2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 500_000; i < 750_000; i++) {
+                    arr[i] = 1.14 * Math.cos(i) * Math.sin(i * 0.2) * Math.cos(i / 1.2);
+                }
+            }
+        });
+        Thread thread3 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 750_000; i < 100_000_000; i++) {
+                    arr[i] = 1.14 * Math.cos(i) * Math.sin(i * 0.2) * Math.cos(i / 1.2);
+                }
+            }
+        });
+        thread.start();
+        thread1.start();
+        thread2.start();
+        thread3.start();
+
+        thread.join();
+        thread1.join();
+        thread2.join();
+        thread3.join();
+
+    }
+}
